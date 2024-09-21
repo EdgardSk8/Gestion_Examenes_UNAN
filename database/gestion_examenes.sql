1e744d6177b8f2373810dc9eeab4c2fe28a7701d
Edgard Tellez\Admin <osais1000@gmail.com>
2024-09-21 13:32:45 -0700

    Cambios realizados

---
+++ database/gestion_examenes.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-09-2024 a las 22:31:55
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `gestion_examenes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `area_conocimiento`
+--
+-- Error leyendo la estructura de la tabla gestion_examenes.area_conocimiento: #1932 - Table 'gestion_examenes.area_conocimiento' doesn't exist in engine
+-- Error leyendo datos de la tabla gestion_examenes.area_conocimiento: #1064 - Algo está equivocado en su sintax cerca 'FROM `gestion_examenes`.`area_conocimiento`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `aulas`
+--
+-- Error leyendo la estructura de la tabla gestion_examenes.aulas: #1932 - Table 'gestion_examenes.aulas' doesn't exist in engine
+-- Error leyendo datos de la tabla gestion_examenes.aulas: #1064 - Algo está equivocado en su sintax cerca 'FROM `gestion_examenes`.`aulas`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `carrera`
+--
+-- Error leyendo la estructura de la tabla gestion_examenes.carrera: #1932 - Table 'gestion_examenes.carrera' doesn't exist in engine
+-- Error leyendo datos de la tabla gestion_examenes.carrera: #1064 - Algo está equivocado en su sintax cerca 'FROM `gestion_examenes`.`carrera`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `departamento`
+--
+-- Error leyendo la estructura de la tabla gestion_examenes.departamento: #1932 - Table 'gestion_examenes.departamento' doesn't exist in engine
+-- Error leyendo datos de la tabla gestion_examenes.departamento: #1064 - Algo está equivocado en su sintax cerca 'FROM `gestion_examenes`.`departamento`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dia_asignado`
+--
+-- Error leyendo la estructura de la tabla gestion_examenes.dia_asignado: #1932 - Table 'gestion_examenes.dia_asignado' doesn't exist in engine
+-- Error leyendo datos de la tabla gestion_examenes.dia_asignado: #1064 - Algo está equivocado en su sintax cerca 'FROM `gestion_examenes`.`dia_asignado`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `edificio`
+--
+-- Error leyendo la estructura de la tabla gestion_examenes.edificio: #1932 - Table 'gestion_examenes.edificio' doesn't exist in engine
+-- Error leyendo datos de la tabla gestion_examenes.edificio: #1064 - Algo está equivocado en su sintax cerca 'FROM `gestion_examenes`.`edificio`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `equipos`
+--
+-- Error leyendo la estructura de la tabla gestion_examenes.equipos: #1932 - Table 'gestion_examenes.equipos' doesn't exist in engine
+-- Error leyendo datos de la tabla gestion_examenes.equipos: #1064 - Algo está equivocado en su sintax cerca 'FROM `gestion_examenes`.`equipos`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `equipos_roles`
+--
+-- Error leyendo la estructura de la tabla gestion_examenes.equipos_roles: #1932 - Table 'gestion_examenes.equipos_roles' doesn't exist in engine
+-- Error leyendo datos de la tabla gestion_examenes.equipos_roles: #1064 - Algo está equivocado en su sintax cerca 'FROM `gestion_examenes`.`equipos_roles`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estudiantes`
+--
+-- Error leyendo la estructura de la tabla gestion_examenes.estudiantes: #1932 - Table 'gestion_examenes.estudiantes' doesn't exist in engine
+-- Error leyendo datos de la tabla gestion_examenes.estudiantes: #1064 - Algo está equivocado en su sintax cerca 'FROM `gestion_examenes`.`estudiantes`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `localidades`
+--
+-- Error leyendo la estructura de la tabla gestion_examenes.localidades: #1932 - Table 'gestion_examenes.localidades' doesn't exist in engine
+-- Error leyendo datos de la tabla gestion_examenes.localidades: #1064 - Algo está equivocado en su sintax cerca 'FROM `gestion_examenes`.`localidades`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `perfil`
+--
+-- Error leyendo la estructura de la tabla gestion_examenes.perfil: #1932 - Table 'gestion_examenes.perfil' doesn't exist in engine
+-- Error leyendo datos de la tabla gestion_examenes.perfil: #1064 - Algo está equivocado en su sintax cerca 'FROM `gestion_examenes`.`perfil`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `profesores`
+--
+-- Error leyendo la estructura de la tabla gestion_examenes.profesores: #1932 - Table 'gestion_examenes.profesores' doesn't exist in engine
+-- Error leyendo datos de la tabla gestion_examenes.profesores: #1064 - Algo está equivocado en su sintax cerca 'FROM `gestion_examenes`.`profesores`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+-- Error leyendo la estructura de la tabla gestion_examenes.rol: #1932 - Table 'gestion_examenes.rol' doesn't exist in engine
+-- Error leyendo datos de la tabla gestion_examenes.rol: #1064 - Algo está equivocado en su sintax cerca 'FROM `gestion_examenes`.`rol`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_examen`
+--
+-- Error leyendo la estructura de la tabla gestion_examenes.tipo_examen: #1932 - Table 'gestion_examenes.tipo_examen' doesn't exist in engine
+-- Error leyendo datos de la tabla gestion_examenes.tipo_examen: #1064 - Algo está equivocado en su sintax cerca 'FROM `gestion_examenes`.`tipo_examen`' en la linea 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
