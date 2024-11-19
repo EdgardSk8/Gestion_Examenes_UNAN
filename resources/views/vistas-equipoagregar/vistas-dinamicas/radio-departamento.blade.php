f916a2c2447f8a55fa2c262e7976350679baa337
Edgard Tellez\Admin <osais1000@gmail.com>
2024-11-19 15:18:48 -0800

    d

--- resources/views/vistas-equipoagregar/vistas-dinamicas/radio-departamento.blade.php
+++ resources/views/vistas-equipoagregar/vistas-dinamicas/radio-departamento.blade.php
@@ -1,7 +1,7 @@
 <!-- resources/views/vistas-equipoagregar/agregardatos/radio-departamento.blade.php -->
 
 
-<div class="agregar-departamento">
+<div class="contenedor-agregar-datos">
     <h2 style="text-align: center">Agregar Departamento</h2>
 
     <form action="{{ route('departamento.agregar') }}" method="POST">
